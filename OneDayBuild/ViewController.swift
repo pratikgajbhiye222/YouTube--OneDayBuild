f6d49fb399787944f07ec82c3327e39fbf3fb466
pratikgajbhiye222 <pratik.gajbhiye222@gmail.com>
2020-06-11 01:17:28 +0530

    Initial Commit

---
+++ OneDayBuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneDayBuild
+//
+//  Created by pratik gajbhiye on 11/06/20.
+//  Copyright © 2020 Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
